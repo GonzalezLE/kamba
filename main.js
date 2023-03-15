f153f4bf6885060a14dd2dadad4fcb99418a2422
Luis Enrique Gonzalez Arellano <gonzalez-3216@hotmail.com>
2023-03-15 00:58:22 -0600

    add

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function dragover(evt) {
     evt.preventDefault();
     evt.dataTransfer.dropEffect = "move";
+   
 }
 
 function dragLeave(evt) {} // available if required
@@ -9,10 +10,18 @@ function dragEnd(evt) {} // available if required
 function dragstart(evt) {
     evt.dataTransfer.setData("text/plain", evt.target.id);
     evt.dataTransfer.effectAllowed = "move";
+   
 }
 
 function drop(evt) {
     evt.preventDefault();
     var data = evt.dataTransfer.getData("text");
     evt.target.closest('.scrum-board-column').appendChild(document.getElementById(data));
+    
+    let son = data;
+    let father = document.getElementById(data).parentNode.id;
+
+    
+    alert(father)
+    alert(son)
 }
